207f02a31d8a9f644600b30b77f0f5c3e943702d
Victor Recabarren <victormrecabarren@Victors-MacBook-Pro.local>
2019-03-09 11:00:37 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,21 @@
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
+var cardsInPlay = [];
+
+var cardOne = cards[3];
+
+cardsInPlay.push(cardOne);
+
+	console.log("User flipped king");
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+	console.log("User flipped king");
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("It's a match!");
+} else {
+	alert("Sorry, try again");
+};
