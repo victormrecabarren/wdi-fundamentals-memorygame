72b376f86fa80870b9603637bff633170ce1bae1
Victor Recabarren <victormrecabarren@gmail.com>
2019-03-11 18:12:22 -0700

    added more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,25 +25,35 @@ var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("It's a match!");
+	alert("It's a match!");
 } else {
-	console.log("Sorry, try again");
+	alert("Sorry, try again");
 };
 };
 
-var flipCard = function (cardId) {
-console.log("User flipped " + cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-cardsInPlay.push(cards[cardId].rank);
-	if (cardsInPlay.length > 1) {
-checkForMatch(); 
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
+		if (cardsInPlay.length > 1) {
+	checkForMatch(); 
 };
 	
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', 'images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement)
+
+	}
+}
+createBoard();
+
+
 
 
 
